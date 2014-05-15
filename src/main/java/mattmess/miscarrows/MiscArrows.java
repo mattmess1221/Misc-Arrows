a3763b1c9294a29a5a92ad603f5928dd1cf6c48c
killjoy1221 <mattmess1221@gmail.com>
2014-05-14 20:10:19 -0400

    first commit

---
+++ src/main/java/mattmess/miscarrows/MiscArrows.java
@@ -0,0 +1,14 @@
+package mattmess.miscarrows;
+
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.Mod.EventHandler;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = "miscarrows", name= "Misc Arrows", version = "1.0")
+public class MiscArrows {
+
+	@EventHandler
+	public void startup(FMLPreInitializationEvent event){
+		
+	}
+}
